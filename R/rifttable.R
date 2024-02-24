36ecafe81b294c6e889d1edf5c38dbb008b1904e
Konrad H. Stopsack <35557324+stopsack@users.noreply.github.com>
2024-02-24 17:19:35 -0500

    table1_design: allow for "@" in factor levels

--- R/rifttable.R
+++ R/rifttable.R
@@ -579,7 +579,8 @@ rifttable <- function(
       tidyr::separate(
         col = "combo",
         into = c("outcome", "var_level"),
-        sep = "@") %>%
+        sep = "@",
+        extra = "merge") %>%
       dplyr::distinct()
     data <- purrr::map2_dfc(
       .x = to_code$outcome,
